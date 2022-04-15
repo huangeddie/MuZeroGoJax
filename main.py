440e1e8b3a95c23cc4e03f22b3ec6d0df48fe821
Eddie Huang <aigagror@gmail.com>
2022-04-14 20:57:57 -0700

    Drafted training loop with random model

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import haiku as hk
+import jax.nn
 from jax import numpy as jnp
 from gojax import go
 from gojax import constants
@@ -6,28 +7,32 @@ from gojax import constants
 
 class GoModel(hk.Module):
     def __call__(self, x):
-        w = hk.get_parameter("w", [], init=jnp.zeros)
-        return jnp.sum(x * w, axis=(1,))
+        rng_key = hk.next_rng_key()
+        return jax.random.normal(rng_key, (x.shape[0], x.shape[2], x.shape[3]))
 
 
 def _call_go_model(states):
     return GoModel()(states)
 
 
-def train(model_fn):
-    states = go.new_states(7, 1)
-    params = model_fn.init(None, states)
-    print(params)
-    return params
+def train(model_fn, rng_key):
+    return model_fn.init(rng_key, go.new_states(7, 1))
 
 
 if __name__ == '__main__':
-    call_go_model = hk.without_apply_rng(hk.transform(_call_go_model))
-    parameters = train(call_go_model)
+    call_go_model = hk.transform(_call_go_model)
+    rng_key = jax.random.PRNGKey(42)
+    parameters = train(call_go_model, rng_key)
 
-    states = go.new_states(7, 1)
-    while(not states[0, constants.END_CHANNEL_INDEX, 0, 0]):
-        action_logits = call_go_model.apply(parameters, states)
-        available_action_logits = action_logits * jnp.array(~states[:, constants.INVALID_CHANNEL_INDEX], dtype=float)
-
-
+    board_size = 7
+    states = go.new_states(board_size, 1)
+    while not states[0, constants.END_CHANNEL_INDEX, 0, 0]:
+        rng_key, sub_key = jax.random.split(rng_key)
+        action_exp_logits = jnp.exp(jax.jit(call_go_model.apply)(parameters, sub_key, states)) * jnp.array(
+            ~states[:, constants.INVALID_CHANNEL_INDEX], dtype=float)
+        flattened_action_values = jnp.reshape(action_exp_logits, (1, -1))
+        action_1d = jnp.argmax(flattened_action_values, axis=-1)
+        one_hot_action_1d = jax.nn.one_hot(action_1d, board_size ** 2, dtype=bool)
+        indicator_actions = jnp.reshape(one_hot_action_1d, (-1, board_size, board_size))
+        states = jax.jit(go.next_states)(states, indicator_actions)
+        print(go.get_pretty_string(states[0]))

cddf59c09cc040a4170c16a6f2831e2727f5a9a9
Eddie Huang <aigagror@gmail.com>
2022-04-17 11:01:39 -0700

    Extracted self play logic into new self_play function

--- main.py
+++ main.py
@@ -30,25 +30,27 @@ def simulate_next_states(go_model, params, rng_key, states):
     return states
 
 
-def main():
-    go_model = hk.transform(_call_go_model)
-    rng_key = jax.random.PRNGKey(42)
-    parameters = train(go_model, rng_key)
-
-    board_size = 7
-    batch_size = 1
+def self_play(go_model, parameters, batch_size, board_size, rng_key):
     states = go.new_states(board_size, batch_size)
     step = 0
     max_num_steps = 2 * (board_size ** 2)
     history = jnp.repeat(jnp.expand_dims(states, axis=1), max_num_steps, 1).at[:, step].set(states)
-
     while not jnp.alltrue(go.get_ended(states)) and step <= max_num_steps:
         states = simulate_next_states(go_model, parameters, rng_key, states)
         rng_key, _ = jax.random.split(rng_key)
         step += 1
         history = history.at[:, step].set(states)
+    return history
 
-    for step in range(max_num_steps):
+
+def main():
+    go_model = hk.transform(_call_go_model)
+    rng_key = jax.random.PRNGKey(42)
+    parameters = train(go_model, rng_key)
+
+    history = self_play(go_model, parameters, batch_size=1, board_size=7, rng_key=rng_key)
+
+    for step in range(history.shape[1]):
         print(f'Step {step}')
         print(go.get_pretty_string(history[0, step]))
 

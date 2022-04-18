3f3ca0ac1b7682e0f67959f3b9bc89efd5a8999f
Eddie Huang <aigagror@gmail.com>
2022-04-17 22:51:28 -0700

    drafted more of the trianing code

--- main.py
+++ main.py
@@ -9,16 +9,8 @@ class GoModel(hk.Module):
         return jax.random.normal(hk.next_rng_key(), (x.shape[0], x.shape[2], x.shape[3]))
 
 
-def _call_go_model(states):
-    return GoModel()(states)
-
-
-def train(model_fn, rng_key):
-    return model_fn.init(rng_key, go.new_states(7, 1))
-
-
-def simulate_next_states(go_model, params, rng_key, states):
-    raw_action_logits = go_model.apply(params, rng_key, states)
+def simulate_next_states(model_fn, params, rng_key, states):
+    raw_action_logits = model_fn.apply(params, rng_key, states)
     action_logits = jnp.where(go.get_invalids(states),
                               jnp.full_like(raw_action_logits, float('-inf')), raw_action_logits)
     flattened_action_values = jnp.reshape(action_logits, (states.shape[0], -1))
@@ -29,25 +21,53 @@ def simulate_next_states(go_model, params, rng_key, states):
     return states
 
 
-def self_play(go_model, params, batch_size, board_size, rng_key):
+def self_play(model_fn, params, batch_size, board_size, rng_key):
     states = go.new_states(board_size, batch_size)
     step = 0
     max_num_steps = 2 * (board_size ** 2)
     history = jnp.repeat(jnp.expand_dims(states, axis=1), max_num_steps, 1).at[:, step].set(states)
     while not jnp.alltrue(go.get_ended(states)) and step <= max_num_steps:
-        states = simulate_next_states(go_model, params, rng_key, states)
+        states = simulate_next_states(model_fn, params, rng_key, states)
         rng_key, _ = jax.random.split(rng_key)
         step += 1
         history = history.at[:, step].set(states)
     return history
 
 
-def main():
-    go_model = hk.transform(_call_go_model)
-    rng_key = jax.random.PRNGKey(42)
-    parameters = train(go_model, rng_key)
+def get_winners(history):
+    raise NotImplementedError()
 
-    history = self_play(go_model, parameters, batch_size=1, board_size=7, rng_key=rng_key)
+
+def update_params(params, history):
+    num_steps = history.shape[1]
+    odd_steps = jnp.arange(num_steps // 2) * 2 + 1
+    white_perspective_negation = jnp.ones((len(history), num_steps)).at[:, odd_steps].set(-1)
+    state_labels = white_perspective_negation * get_winners(history)
+    NotImplementedError()
+    return params
+
+
+def train(model_fn, batch_size, board_size, epochs, rng_key):
+    params = model_fn.init(rng_key, go.new_states(board_size, 1))
+
+    for _ in range(epochs):
+        history = self_play(model_fn, params, batch_size, board_size)
+        params = update_params(params, history)
+
+    return params
+
+
+def main():
+    go_model = hk.transform(lambda states: GoModel()(states))
+
+    batch_size = 1
+    board_size = 7
+    epochs = 1
+    rng_key = jax.random.PRNGKey(42)
+    parameters = train(go_model, batch_size, board_size, epochs, rng_key)
+
+    single_batch_size = 1
+    history = self_play(go_model, parameters, single_batch_size, board_size, rng_key)
 
     for step in range(history.shape[1]):
         print(f'Step {step}')

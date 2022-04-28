320b4bbe35f5a5742b358f98f50cf46565113714
Eddie Huang <aigagror@gmail.com>
2022-04-27 19:48:55 -0700

    Add tests for get_winners

--- tests/test_game.py
+++ tests/test_game.py
@@ -101,6 +101,26 @@ class MyTestCase(chex.TestCase):
         expected_trajectories = _read_trajectory('random_self_play_3x3_42rng_expected_trajectory.txt')
         np.testing.assert_array_equal(trajectories, expected_trajectories)
 
+    def test_get_winners_one_tie_one_winning_one_winner(self):
+        trajectories = game.new_trajectories(board_size=3, batch_size=3, max_num_steps=2)
+        trajectories = trajectories.at[:1, 1].set(gojax.decode_state("""
+                                                                    _ _ _
+                                                                    _ _ _
+                                                                    _ _ _
+                                                                    """, ended=False))
+        trajectories = trajectories.at[1:2, 1].set(gojax.decode_state("""
+                                                                    _ _ _
+                                                                    _ B _
+                                                                    _ _ _
+                                                                    """, ended=False))
+        trajectories = trajectories.at[2:3, 1].set(gojax.decode_state("""
+                                                                    _ _ _
+                                                                    _ B _
+                                                                    _ _ _
+                                                                    """, ended=True))
+        winners = game.get_winners(trajectories)
+        np.testing.assert_array_equal(winners, [0, 1, 1])
+
 
 if __name__ == '__main__':
     unittest.main()

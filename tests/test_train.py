c11d52b54e122a99089e3ad794736e66c06f4998
Eddie Huang <aigagror@gmail.com>
2022-05-08 16:04:00 -0700

    Pylint disable no-value-for-parameter caused by @chex.variants decorator

--- tests/test_train.py
+++ tests/test_train.py
@@ -1,5 +1,5 @@
 """Tests train.py."""
-# pylint: disable=missing-function-docstring,no-self-use,unnecessary-lambda
+# pylint: disable=missing-function-docstring,no-self-use,unnecessary-lambda,no-value-for-parameter
 import copy
 import unittest
 

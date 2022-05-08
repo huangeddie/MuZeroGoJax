7ac07dba2ce57a09c19cec3ebda983c2a4d53f3a
Eddie Huang <aigagror@gmail.com>
2022-05-08 15:47:43 -0700

    Parameterize and JIT-vary the value step tests for the linear model.

--- tests/test_train.py
+++ tests/test_train.py
@@ -39,6 +39,52 @@ class ValueLossFnLinearTestCase(chex.TestCase):
                                expected_loss)
 
 
+class LinearValueStepParameterizedTestCase(chex.TestCase):
+    """Tests the value step under the linear model."""
+
+    @chex.variants(with_jit=True, without_jit=True)
+    @parameterized.named_parameters(
+        {'testcase_name': '_zeros_params_with_single_empty_state', 'params_fill_value': 0,
+         'state': gojax.new_states(batch_size=1, board_size=3),
+         'expected_value_w': jnp.zeros((gojax.NUM_CHANNELS, 3, 3)), 'expected_value_b': 0},
+        {'testcase_name': '_ones_params_with_single_empty_state', 'params_fill_value': 1,
+         'state': gojax.new_states(batch_size=1, board_size=3),
+         'expected_value_w': jnp.ones((gojax.NUM_CHANNELS, 3, 3)), 'expected_value_b': 0.768941},
+        {'testcase_name': '_ones_params_with_single_black_piece', 'params_fill_value': 1,
+         'state': gojax.decode_state("""
+                                    _ _ _
+                                    _ B _
+                                    _ _ _
+                                    """),
+         'expected_value_w': jnp.ones((gojax.NUM_CHANNELS, 3, 3)).at[
+             gojax.BLACK_CHANNEL_INDEX, 1, 1].set(1.047426).at[
+             gojax.INVALID_CHANNEL_INDEX, 1, 1].set(1.047426), 'expected_value_b': 1.047426},
+        {'testcase_name': '_ones_params_with_single_white_piece', 'params_fill_value': 1,
+         'state': gojax.decode_state("""
+                                    _ _ _
+                                    _ W _
+                                    _ _ _
+                                    """),
+         'expected_value_w': jnp.ones((gojax.NUM_CHANNELS, 3, 3)).at[
+             gojax.WHITE_CHANNEL_INDEX, 1, 1].set(0.047425866).at[
+             gojax.INVALID_CHANNEL_INDEX, 1, 1].set(0.047425866), 'expected_value_b': 0.047425866},
+    )
+    def test(self, params_fill_value, state, expected_value_w, expected_value_b):
+        linear_model = models.get_model('linear')
+        params = linear_model.init(jax.random.PRNGKey(42), state)
+        linear_params = params['linear_go_model']
+        linear_params['value_w'] = jnp.full_like(linear_params['value_w'], params_fill_value)
+        linear_params['value_b'] = jnp.full_like(linear_params['value_b'], params_fill_value)
+
+        value_step_fn = self.variant(jax.tree_util.Partial(train.value_step, linear_model))
+        new_params = value_step_fn(params, jnp.expand_dims(state, axis=1), learning_rate=1)
+
+        expected_params = copy.copy(params)
+        expected_params['linear_go_model']['value_w'] = expected_value_w
+        expected_params['linear_go_model']['value_b'] = expected_value_b
+        chex.assert_trees_all_close(new_params, expected_params)
+
+
 class TrainTestCase(unittest.TestCase):
     """Tests train.py."""
 
@@ -47,7 +93,6 @@ class TrainTestCase(unittest.TestCase):
         states = jnp.ones_like(gojax.new_states(batch_size=1, board_size=3))
         params = linear_model.init(jax.random.PRNGKey(42), states)
         linear_params = params['linear_go_model']
-        linear_params['action_w'] = jnp.ones_like(linear_params['action_w'])
         linear_params['value_w'] = jnp.ones_like(linear_params['value_w'])
         linear_params['value_b'] = jnp.ones_like(linear_params['value_b'])
 
@@ -70,93 +115,6 @@ class TrainTestCase(unittest.TestCase):
             params['linear_go_model']['transition_b'])
 
         chex.assert_trees_all_close(grads, expected_grad)
-
-    def test_value_step_zeros_linear_with_single_empty_state(self):
-        linear_model = models.get_model('linear')
-        states = gojax.new_states(batch_size=1, board_size=3)
-        params = linear_model.init(jax.random.PRNGKey(42), states)
-        linear_params = params['linear_go_model']
-        linear_params['action_w'] = jnp.zeros_like(linear_params['action_w'])
-        linear_params['value_w'] = jnp.zeros_like(linear_params['value_w'])
-        linear_params['value_b'] = jnp.zeros_like(linear_params['value_b'])
-
-        new_params = train.value_step(linear_model, params, jnp.expand_dims(states, axis=1),
-                                      learning_rate=0.1)
-
-        chex.assert_trees_all_equal(new_params, params)
-
-    def test_value_step_ones_linear_with_single_empty_state(self):
-        linear_model = models.get_model('linear')
-        states = gojax.new_states(batch_size=1, board_size=3)
-        params = linear_model.init(jax.random.PRNGKey(42), states)
-        linear_params = params['linear_go_model']
-        linear_params['action_w'] = jnp.ones_like(linear_params['action_w'])
-        linear_params['value_w'] = jnp.ones_like(linear_params['value_w'])
-        linear_params['value_b'] = jnp.ones_like(linear_params['value_b'])
-
-        new_params = train.value_step(linear_model, params, jnp.expand_dims(states, axis=1),
-                                      learning_rate=1)
-
-        # Decrease value parameters towards 0.
-        expected_params = copy.copy(params)
-        expected_params['linear_go_model']['value_b'] = 0.768941
-        chex.assert_trees_all_close(new_params, expected_params)
-
-    def test_value_step_ones_linear_with_single_black_piece(self):
-        linear_model = models.get_model('linear')
-        states = gojax.decode_state("""
-                                    _ _ _
-                                    _ B _
-                                    _ _ _
-                                    """)
-        params = linear_model.init(jax.random.PRNGKey(42), states)
-        linear_params = params['linear_go_model']
-        linear_params['action_w'] = jnp.ones_like(linear_params['action_w'])
-        linear_params['value_w'] = jnp.ones_like(linear_params['value_w'])
-        linear_params['value_b'] = jnp.ones_like(linear_params['value_b'])
-
-        new_params = train.value_step(linear_model, params, jnp.expand_dims(states, axis=1),
-                                      learning_rate=1)
-
-        # Increase value parameters at the index corresponding to the black channel and invalid
-        # moves channel.
-        # Sigmoid output should be sigmoid(2 + 1) ~ 0.95257. Derivative w.r.t logit is sigmoid -
-        # label ~ 047426.
-        expected_params = copy.copy(params)
-        expected_params['linear_go_model']['value_w'] = params['linear_go_model']['value_w'].at[
-            gojax.BLACK_CHANNEL_INDEX, 1, 1].set(1.047426).at[
-            gojax.INVALID_CHANNEL_INDEX, 1, 1].set(1.047426)
-        expected_params['linear_go_model']['value_b'] = 1.047426
-
-        chex.assert_trees_all_close(new_params, expected_params)
-
-    def test_value_step_ones_linear_with_single_white_piece(self):
-        linear_model = models.get_model('linear')
-        states = gojax.decode_state("""
-                                _ _ _
-                                _ W _
-                                _ _ _
-                                """)
-        params = linear_model.init(jax.random.PRNGKey(42), states)
-        linear_params = params['linear_go_model']
-        linear_params['action_w'] = jnp.ones_like(linear_params['action_w'])
-        linear_params['value_w'] = jnp.ones_like(linear_params['value_w'])
-        linear_params['value_b'] = jnp.ones_like(linear_params['value_b'])
-
-        new_params = train.value_step(linear_model, params, jnp.expand_dims(states, axis=1),
-                                      learning_rate=1)
-
-        # Decrease value parameters at the index corresponding to the black channel and invalid
-        # moves channel.
-        # Sigmoid output should be sigmoid(2 + 1) ~ 0.95257. Derivative w.r.t logit is sigmoid -
-        # label ~ 0.95257.
-        expected_params = copy.copy(params)
-        expected_params['linear_go_model']['value_w'] = \
-            params['linear_go_model']['value_w'].at[gojax.WHITE_CHANNEL_INDEX, 1, 1].set(
-                0.047426).at[
-                gojax.INVALID_CHANNEL_INDEX, 1, 1].set(0.047426)
-        expected_params['linear_go_model']['value_b'] = 0.047426
-        chex.assert_trees_all_close(new_params, expected_params, rtol=1e-5)
 
     if __name__ == '__main__':
         unittest.main()

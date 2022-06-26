6cb827ee3cd11c3dcbf45b0e0ce5d1d618c13eb5
Eddie Huang <aigagror@gmail.com>
2022-06-26 16:06:22 -0700

    Add layer norm to simple conv block

--- src/muzero_gojax/models/base.py
+++ src/muzero_gojax/models/base.py
@@ -18,7 +18,9 @@ class SimpleConvBlock(hk.Module):
     def __init__(self, hdim, odim, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._conv1 = hk.Conv2D(hdim, (3, 3), data_format='NCHW')
+        self._layer_norm = hk.LayerNorm(axis=(1, 2, 3), create_scale=False, create_offset=False)
         self._conv2 = hk.Conv2D(odim, (3, 3), data_format='NCHW')
 
     def __call__(self, input_3d):
-        return self._conv2(jax.nn.relu(self._conv1(input_3d.astype('bfloat16'))))
+        return self._layer_norm(
+            self._conv2(jax.nn.relu(self._layer_norm(self._conv1(input_3d.astype('bfloat16'))))))

b6d16c5532fe2583e69bc7733fa0d288ebb794d1
Eddie Huang <aigagror@gmail.com>
2022-07-10 17:49:24 -0700

    Extract loss functions into separate module

---
+++ src/muzero_gojax/losses.py
@@ -0,0 +1,192 @@
+import jax.nn
+import jax.tree_util
+from jax import numpy as jnp, lax
+
+
+def nd_categorical_cross_entropy(x_logits, y_logits, temp=None, mask=None):
+    """
+    Categorical cross-entropy with respect to the last dimension.
+
+    :param x_logits: (N*) x D float array
+    :param y_logits: (N*) x D float array
+    :param temp: temperature constant
+    :param mask: 0-1 mask to determine which logits to consider.
+    :return: Mean cross-entropy loss between the softmax of x and softmax of (y / temp)
+    """
+    if temp is None:
+        temp = 1
+    if mask is None:
+        mask = jnp.ones(x_logits.shape[:-1])
+    cross_entropy = -jnp.sum(jax.nn.softmax(y_logits / temp) * jax.nn.log_softmax(x_logits),
+                             axis=-1)
+
+    return jnp.sum(cross_entropy * mask) / jnp.sum(mask, dtype='bfloat16')
+
+
+def sigmoid_cross_entropy(value_logits, labels, mask=None):
+    """
+    Computes the sigmoid cross-entropy given binary labels and logit values.
+
+    :param value_logits: N^D array of float values
+    :param labels: N^D array of N binary (0, 1) values
+    :param mask: 0-1 mask to determine which logits to consider.
+    :return: Mean cross-entropy loss between the sigmoid of the value logits and the labels.
+    """
+    if mask is None:
+        mask = jnp.ones_like(value_logits)
+    cross_entropy = -labels * jax.nn.log_sigmoid(value_logits) - (1 - labels) * jax.nn.log_sigmoid(
+        -value_logits)
+    return jnp.sum(cross_entropy * mask) / jnp.sum(mask, dtype='bfloat16')
+
+
+def make_first_k_steps_mask(batch_size, total_steps, k):
+    """Creates a boolean mask of shape batch_size x total_steps, where the first k steps are True
+    and the rest are false."""
+    return jnp.repeat(jnp.expand_dims(jnp.arange(total_steps) < k, 0), batch_size, axis=0)
+
+
+def compute_policy_loss(policy_model, value_model, params, i, transitions, nt_embeds):
+    """
+    Computes the softmax cross entropy loss using value_model(transitions) as the labels and the
+    policy_model(nt_embeddings) as the training logits.
+
+    To prevent training the value model, the gradient flow is cut off from the value model.
+
+    :param policy_model: Policy model.
+    :param value_model: Value model.
+    :param params: Parameters.
+    :param i: Iteration index when this function is used in fori_loops.
+    :param transitions: N x T x A x (D^m) array where D^m represents the Go embedding shape.
+    :param nt_embeds: N x T x (D^m) array where D^m represents the Go embedding shape.
+    :return: Scalar float value.
+    """
+    # pylint: disable=too-many-arguments
+    batch_size, total_steps, action_size = transitions.shape[:3]
+    embed_shape = transitions.shape[3:]
+    num_examples = batch_size * total_steps
+    # transition_value_logits is a 1-D vector of length N * T * A.
+    flat_transition_value_logits = value_model(params, None, jnp.reshape(transitions, (
+        num_examples * action_size,) + embed_shape))
+    trajectory_policy_shape = (batch_size, total_steps, action_size)
+    transition_value_logits = jnp.reshape(flat_transition_value_logits, trajectory_policy_shape)
+    policy_logits = policy_model(params, None,
+                                 jnp.reshape(nt_embeds, (num_examples,) + embed_shape))
+    return nd_categorical_cross_entropy(jnp.reshape(policy_logits, trajectory_policy_shape),
+                                        lax.stop_gradient(transition_value_logits),
+                                        mask=make_first_k_steps_mask(batch_size, total_steps,
+                                                                     total_steps - i))
+
+
+def compute_value_loss(value_model, params, i, nt_embeds, nt_game_winners):
+    """
+    Computes the binary cross entropy loss between sigmoid(value_model(nt_embeds)) and
+    nt_game_winners.
+
+    :param value_model: Value model.
+    :param params: Parameters of value model.
+    :param i: i'th hypothetical step.
+    :param nt_embeds: An N x T x (D*) array of Go state embeddings.
+    :param nt_game_winners: An N x T integer array of length N. 1 = black won, 0 = tie,
+    -1 = white won.
+    :return: Scalar float value.
+    """
+    batch_size, total_steps = nt_embeds.shape[:2]
+    embed_shape = nt_embeds.shape[2:]
+    num_examples = batch_size * total_steps
+    labels = (jnp.roll(nt_game_winners, shift=i) + 1) / 2
+    flat_value_logits = value_model(params, None,
+                                    jnp.reshape(nt_embeds, (num_examples,) + embed_shape))
+    return sigmoid_cross_entropy(jnp.reshape(flat_value_logits, (batch_size, total_steps)), labels,
+                                 mask=make_first_k_steps_mask(batch_size, total_steps,
+                                                              total_steps - i))
+
+
+def update_k_step_losses(model_fn, params, i, data):
+    """
+    Updates data to the i'th hypothetical step and adds the corresponding value and policy losses
+    at that step.
+
+    :param model_fn: Haiku model architecture.
+    :param params: Parameters of the model.
+    :param i: The index of the hypothetical step (0-indexed).
+    :param data: A dictionary structure of the format
+        'nt_embeds': An N x T x (D*) array of Go state embeddings.
+        'nt_actions': An N x T non-negative integer array.
+        'nt_game_winners': An N x T integer array of length N. 1 = black won, 0 = tie, -1 = white
+        won.
+        'cum_val_loss': Cumulative value loss.
+    :return: An updated version of data.
+    """
+    _, value_model, policy_model, transition_model = model_fn.apply
+    batch_size, total_steps = data['nt_embeds'].shape[:2]
+    num_examples = batch_size * total_steps
+    embed_shape = data['nt_embeds'].shape[2:]
+
+    # Update the cumulative value loss.
+    data['cum_val_loss'] += compute_value_loss(value_model, params, i, data['nt_embeds'],
+                                               data['nt_game_winners'])
+
+    # Get the transitions.
+    # Flattened transitions is (N * T) x A x (D*)
+    flat_transitions = transition_model(params, None, jnp.reshape(data['nt_embeds'],
+                                                                  (num_examples,) + embed_shape))
+    transitions = jnp.reshape(flat_transitions,
+                              (batch_size, total_steps, flat_transitions.shape[1]) + embed_shape)
+
+    # Update the cumulative policy loss.
+    data['cum_policy_loss'] += compute_policy_loss(policy_model, value_model, params, i,
+                                                   transitions, data['nt_embeds'])
+
+    # Update the state embeddings from the transitions indexed by the played actions.
+    flat_next_states = flat_transitions[
+        jnp.arange(num_examples), jnp.reshape(data['nt_actions'], num_examples)]
+    data['nt_embeds'] = jnp.roll(
+        jnp.reshape(flat_next_states, (batch_size, total_steps) + embed_shape), -1, axis=1)
+
+    return data
+
+
+def compute_k_step_losses(model_fn, params, trajectories, actions, game_winners, k=1):
+    # pylint: disable=too-many-arguments
+    """
+    Computes the value, and policy k-step losses.
+
+    :param model_fn: Haiku model architecture.
+    :param params: Parameters of the model.
+    :param trajectories: An N x T X C X H x W boolean array.
+    :param actions: An N x T non-negative integer array.
+    :param game_winners: An N x T integer array of length N. 1 = black won, 0 = tie, -1 = white won.
+    :param k: Number of hypothetical steps.
+    :return: A dictionary of cumulative losses.
+    """
+    embed_model = model_fn.apply[0]
+    batch_size, total_steps, channels, height, width = trajectories.shape
+    embeddings = embed_model(params, None, jnp.reshape(trajectories, (
+        batch_size * total_steps, channels, height, width)))
+    embed_shape = embeddings.shape[1:]
+    data = lax.fori_loop(lower=0, upper=k,
+                         body_fun=jax.tree_util.Partial(update_k_step_losses, model_fn, params),
+                         init_val={'nt_embeds': jnp.reshape(embeddings, (
+                             batch_size, total_steps) + embed_shape), 'nt_actions': actions,
+                                   'nt_game_winners': game_winners, 'cum_val_loss': 0,
+                                   'cum_policy_loss': 0})
+    return {key: data[key] for key in ['cum_val_loss', 'cum_policy_loss']}
+
+
+def compute_k_step_total_loss(model_fn, params, trajectories, actions, game_winners, k=1):
+    # pylint: disable=too-many-arguments
+    """
+    Computes the sum of all losses.
+
+    Use this function to compute the gradient of the model parameters.
+
+    :param model_fn: Haiku model architecture.
+    :param params: Parameters of the model.
+    :param trajectories: An N x T X C X H x W boolean array.
+    :param actions: An N x T non-negative integer array.
+    :param game_winners: An N x T integer array of length N. 1 = black won, 0 = tie, -1 = white won.
+    :param k: Number of hypothetical steps.
+    :return: A dictionary of cumulative losses.
+    """
+    loss_dict = compute_k_step_losses(model_fn, params, trajectories, actions, game_winners, k)
+    return loss_dict['cum_val_loss'] + loss_dict['cum_policy_loss'], loss_dict
--- tests/test_losses.py
+++ tests/test_losses.py
@@ -11,8 +11,8 @@ import numpy as np
 from absl.testing import parameterized
 from jax import numpy as jnp
 
+from muzero_gojax import losses
 from muzero_gojax import models
-from muzero_gojax import train
 
 
 class LossFunctionsTestCase(chex.TestCase):
@@ -24,8 +24,8 @@ class LossFunctionsTestCase(chex.TestCase):
                                     ('zero_one_one_zero', [[0, 1]], [[1, 0]], 1.04432),
                                     ('zero_one', [[0, 1]], [[0, 1]], 0.582203),
                                     # Average of 0.693147 and 0.582203
-                                    (
-                                    'batch_size_two', [[1, 1], [0, 1]], [[1, 1], [0, 1]], 0.637675),
+                                    ('batch_size_two', [[1, 1], [0, 1]], [[1, 1], [0, 1]],
+                                     0.637675),
                                     ('three_logits_correct', [[0, 1, 0]], [[0, 1, 0]], 0.975328),
                                     ('three_logits_correct', [[0, 0, 1]], [[0, 0, 1]], 0.975328),
                                     ('cold_temperature', [[0, 0, 1]], [[0, 0, 1]], 0.764459, 0.5),
@@ -36,9 +36,9 @@ class LossFunctionsTestCase(chex.TestCase):
     def test_nd_categorical_cross_entropy(self, action_logits, transition_value_logits,
                                           expected_loss, temp=None):
         np.testing.assert_allclose(
-            self.variant(train.nd_categorical_cross_entropy)(jnp.array(action_logits),
-                                                             jnp.array(transition_value_logits),
-                                                             temp), expected_loss, rtol=1e-6)
+            self.variant(losses.nd_categorical_cross_entropy)(jnp.array(action_logits),
+                                                              jnp.array(transition_value_logits),
+                                                              temp), expected_loss, rtol=1e-6)
 
     @chex.variants(with_jit=True, without_jit=True)
     @parameterized.named_parameters(('zero_tie', [0], [0.5], 0.693147),
@@ -53,7 +53,7 @@ class LossFunctionsTestCase(chex.TestCase):
                                     ('neg_two_black', [-2], [0], 0.126928), )
     def test_sigmoid_cross_entropy(self, value_logits, labels, expected_loss):
         np.testing.assert_allclose(
-            self.variant(train.sigmoid_cross_entropy)(jnp.array(value_logits), jnp.array(labels)),
+            self.variant(losses.sigmoid_cross_entropy)(jnp.array(value_logits), jnp.array(labels)),
             expected_loss, rtol=1e-6)
 
     @parameterized.named_parameters(('low_loss', [[[1, 0]]], [[[1, 0]]], 0.582203),
@@ -68,8 +68,8 @@ class LossFunctionsTestCase(chex.TestCase):
         transitions = jnp.array([[[0, 0]]])
         nt_embeds = jnp.array([[[0]]])
         np.testing.assert_allclose(
-            train.compute_policy_loss(policy_mock_model, value_mock_model, params, step,
-                                      transitions, nt_embeds), expected_loss, rtol=1e-6)
+            losses.compute_policy_loss(policy_mock_model, value_mock_model, params, step,
+                                       transitions, nt_embeds), expected_loss, rtol=1e-6)
 
     def test_compute_policy_loss_only_policy_grad(self):
         board_size = 2
@@ -82,8 +82,9 @@ class LossFunctionsTestCase(chex.TestCase):
         params.update(
             value_model.init(jax.random.PRNGKey(42), jnp.reshape(jnp.array(0), (1, 1, 1, 1))))
         step = 0
-        grad = jax.grad(train.compute_policy_loss, argnums=2)(policy_model.apply, value_model.apply,
-                                                              params, step, transitions, nt_embeds)
+        grad = jax.grad(losses.compute_policy_loss, argnums=2)(policy_model.apply,
+                                                               value_model.apply, params, step,
+                                                               transitions, nt_embeds)
         self.assertTrue(jnp.alltrue(grad['linear3_d_policy']['action_w'].astype(bool)))
         self.assertTrue(jnp.alltrue(~grad['linear3_d_value']['value_w'].astype(bool)))
         self.assertTrue(jnp.alltrue(~grad['linear3_d_value']['value_b'].astype(bool)))
@@ -99,7 +100,7 @@ class LossFunctionsTestCase(chex.TestCase):
         nt_embeds = jnp.zeros((1, 1, 1))
         nt_game_winners = jnp.array(nt_game_winners)
         np.testing.assert_allclose(
-            train.compute_value_loss(value_mock_model, params, step, nt_embeds, nt_game_winners),
+            losses.compute_value_loss(value_mock_model, params, step, nt_embeds, nt_game_winners),
             expected_loss, rtol=1e-6)
 
     def test_compute_value_loss_grad(self):
@@ -110,8 +111,8 @@ class LossFunctionsTestCase(chex.TestCase):
         nt_game_winners = jnp.ones((1, 1, 5, 1, 1, 1))
         params = value_model.init(jax.random.PRNGKey(42), jnp.reshape(jnp.array(0), (1, 1, 1, 1)))
         step = 0
-        grad = jax.grad(train.compute_value_loss, argnums=1)(value_model.apply, params, step,
-                                                             nt_embeds, nt_game_winners)
+        grad = jax.grad(losses.compute_value_loss, argnums=1)(value_model.apply, params, step,
+                                                              nt_embeds, nt_game_winners)
         self.assertTrue(jnp.alltrue(grad['linear3_d_value']['value_w'].astype(bool)))
         self.assertTrue(jnp.alltrue(grad['linear3_d_value']['value_b'].astype(bool)))
 
@@ -170,9 +171,9 @@ class LossFunctionsTestCase(chex.TestCase):
                 embed_mock_model, value_mock_model, policy_mock_model, transition_mock_model,)
 
             # Execute the loss function.
-            loss_dict = train.compute_k_step_losses(mock_model, {}, jnp.expand_dims(
+            loss_dict = losses.compute_k_step_losses(mock_model, {}, jnp.expand_dims(
                 gojax.new_states(board_size=3, batch_size=1), 0), jnp.array([[-1]]),
-                                                    jnp.array(game_winners))
+                                                     jnp.array(game_winners))
             # Test.
             self.assertLen(loss_dict, 2)
             self.assertIn('cum_val_loss', loss_dict)
@@ -185,6 +186,20 @@ class LossFunctionsTestCase(chex.TestCase):
             self.assertEqual(value_mock_model.call_count, 4)
             self.assertEqual(policy_mock_model.call_count, 2)
             self.assertEqual(transition_mock_model.call_count, 2)
+
+    @parameterized.named_parameters(('zero', 1, 1, 0, [[False]]), ('one', 1, 1, 1, [[True]]),
+                                    ('zeros', 1, 2, 0, [[False, False]]),
+                                    ('half', 1, 2, 1, [[True, False]]),
+                                    ('full', 1, 2, 2, [[True, True]]),
+                                    ('b2_zero', 2, 1, 0, [[False], [False]]),
+                                    ('b2_one', 2, 1, 1, [[True], [True]]),
+                                    ('b2_zeros', 2, 2, 0, [[False, False], [False, False]]),
+                                    ('b2_half', 2, 2, 1, [[True, False], [True, False]]),
+                                    ('b2_full', 2, 2, 2, [[True, True], [True, True]]), )
+    def test_k_steps_mask_(self, batch_size, total_steps, k, expected_output):
+        """Tests the make_first_k_steps_mask based on inputs and expected output."""
+        np.testing.assert_array_equal(losses.make_first_k_steps_mask(batch_size, total_steps, k),
+                                      expected_output)
 
     if __name__ == '__main__':
         unittest.main()

4edaaa91ad902622a5836002f7b8f2e6c6cf2f31
Eddie Huang <aigagror@gmail.com>
2022-10-25 12:15:09 -0400

    Fix train step log bug

--- src/muzero_gojax/train.py
+++ src/muzero_gojax/train.py
@@ -117,7 +117,7 @@ def train_model(go_model: hk.MultiTransformed, params: optax.Params, board_size)
 
     train_data = TrainData(params, opt_state, metrics.Metrics(), rng_key)
     train_step_fn = jax.tree_util.Partial(train_step, board_size, go_model, optimizer)
-    for multi_step in range(_TRAINING_STEPS.value // _EVAL_FREQUENCY.value):
+    for multi_step in range(1, _TRAINING_STEPS.value // _EVAL_FREQUENCY.value + 1):
         train_data = _multiple_train_steps(train_step_fn, _EVAL_FREQUENCY.value, train_data)
         train_history = train_history.at[multi_step].set(train_data.metrics_data)
         timestamp = time.strftime("%H:%M:%S", time.localtime())

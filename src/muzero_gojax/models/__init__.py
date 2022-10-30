13121a5ae5372576d1cc032a739de43dfce95094
Eddie Huang <aigagror@gmail.com>
2022-10-30 16:39:55 -0700

    Remove CNNLite and Resnet transition models

--- src/muzero_gojax/models/__init__.py
+++ src/muzero_gojax/models/__init__.py
@@ -18,31 +18,36 @@ from muzero_gojax.models import policy
 from muzero_gojax.models import transition
 from muzero_gojax.models import value
 
-_EMBED_MODEL = flags.DEFINE_enum('embed_model', 'linear_conv',
-                                 ['black_perspective', 'identity', 'amplified', 'linear_conv',
-                                  'cnn_lite', 'black_cnn_lite', 'resnet'],
-                                 'State embedding model architecture.')
-_DECODE_MODEL = flags.DEFINE_enum('decode_model', 'linear_conv',
-                                  ['amplified', 'scale', 'resnet', 'linear_conv'],
-                                  'State decoding model architecture.')
-_VALUE_MODEL = flags.DEFINE_enum('value_model', 'linear_conv',
-                                 ['random', 'linear', 'linear_conv', 'cnn_lite', 'resnet',
-                                  'tromp_taylor'], 'Value model architecture.')
-_POLICY_MODEL = flags.DEFINE_enum('policy_model', 'linear_conv',
-                                  ['random', 'linear', 'linear_conv', 'cnn_lite', 'resnet',
-                                   'tromp_taylor'], 'Policy model architecture.')
-_TRANSITION_MODEL = flags.DEFINE_enum('transition_model', 'linear_conv',
-                                      ['real', 'black_perspective', 'random', 'linear_conv',
-                                       'cnn_lite', 'resnet', 'resnet_action'],
-                                      'Transition model architecture.')
+_EMBED_MODEL = flags.DEFINE_enum('embed_model', 'linear_conv', [
+    'black_perspective', 'identity', 'amplified', 'linear_conv', 'cnn_lite',
+    'black_cnn_lite', 'resnet'
+], 'State embedding model architecture.')
+_DECODE_MODEL = flags.DEFINE_enum(
+    'decode_model', 'linear_conv',
+    ['amplified', 'scale', 'resnet', 'linear_conv'],
+    'State decoding model architecture.')
+_VALUE_MODEL = flags.DEFINE_enum(
+    'value_model', 'linear_conv',
+    ['random', 'linear', 'linear_conv', 'cnn_lite', 'resnet', 'tromp_taylor'],
+    'Value model architecture.')
+_POLICY_MODEL = flags.DEFINE_enum(
+    'policy_model', 'linear_conv',
+    ['random', 'linear', 'linear_conv', 'cnn_lite', 'resnet', 'tromp_taylor'],
+    'Policy model architecture.')
+_TRANSITION_MODEL = flags.DEFINE_enum(
+    'transition_model', 'linear_conv',
+    ['real', 'black_perspective', 'random', 'linear_conv', 'resnet_action'],
+    'Transition model architecture.')
 
 _HDIM = flags.DEFINE_integer('hdim', 32, 'Hidden dimension size.')
-_NLAYERS = flags.DEFINE_integer('nlayers', 1, 'Number of layers. Applicable to ResNetV2 models.')
+_NLAYERS = flags.DEFINE_integer(
+    'nlayers', 1, 'Number of layers. Applicable to ResNetV2 models.')
 _EMBED_DIM = flags.DEFINE_integer('embed_dim', 6, 'Embedded dimension size.')
 
-_LOAD_DIR = flags.DEFINE_string('load_dir', None, 'File path to load the saved parameters.'
-                                                  'Otherwise the model starts from randomly '
-                                                  'initialized weights.')
+_LOAD_DIR = flags.DEFINE_string(
+    'load_dir', None, 'File path to load the saved parameters.'
+    'Otherwise the model starts from randomly '
+    'initialized weights.')
 
 EMBED_INDEX = 0
 DECODE_INDEX = 1
@@ -69,50 +74,65 @@ def build_model(board_size: int) -> Tuple[hk.MultiTransformed, optax.Params]:
     (2) a policy model, (3) a transition model, and (4) a value model.
     """
 
-    model_build_params = base.ModelBuildParams(board_size, _HDIM.value, _NLAYERS.value,
-                                               _EMBED_DIM.value, _EMBED_MODEL.value,
-                                               _DECODE_MODEL.value, _VALUE_MODEL.value,
-                                               _POLICY_MODEL.value, _TRANSITION_MODEL.value)
+    model_build_params = base.ModelBuildParams(
+        board_size, _HDIM.value, _NLAYERS.value, _EMBED_DIM.value,
+        _EMBED_MODEL.value, _DECODE_MODEL.value, _VALUE_MODEL.value,
+        _POLICY_MODEL.value, _TRANSITION_MODEL.value)
 
     go_model = build_model_transform(model_build_params)
     if _LOAD_DIR.value:
-        params = load_tree_array(os.path.join(_LOAD_DIR.value, 'params.npz'), dtype='bfloat16')
+        params = load_tree_array(os.path.join(_LOAD_DIR.value, 'params.npz'),
+                                 dtype='bfloat16')
         print(f"Loaded parameters from '{_LOAD_DIR.value}'.")
     else:
-        params = go_model.init(jax.random.PRNGKey(42), gojax.new_states(board_size, 1))
+        params = go_model.init(jax.random.PRNGKey(42),
+                               gojax.new_states(board_size, 1))
         print("Initialized parameters randomly.")
     return go_model, params
 
 
-def build_model_transform(model_build_params: base.ModelBuildParams) -> hk.MultiTransformed:
+def build_model_transform(
+        model_build_params: base.ModelBuildParams) -> hk.MultiTransformed:
     """Builds a multi-transformed Go model."""
 
     def f():
         # pylint: disable=invalid-name
         embed_model = {
-            'identity': embed.IdentityEmbed, 'linear_conv': embed.LinearConvEmbed,
-            'amplified': embed.AmplifiedEmbed, 'black_perspective': embed.BlackPerspectiveEmbed,
-            'black_cnn_lite': embed.BlackCnnLiteEmbed, 'cnn_lite': embed.CnnLiteEmbed,
+            'identity': embed.IdentityEmbed,
+            'linear_conv': embed.LinearConvEmbed,
+            'amplified': embed.AmplifiedEmbed,
+            'black_perspective': embed.BlackPerspectiveEmbed,
+            'black_cnn_lite': embed.BlackCnnLiteEmbed,
+            'cnn_lite': embed.CnnLiteEmbed,
             'resnet': embed.ResNetV2Embed,
         }[model_build_params.embed_model_key](model_build_params)
         decode_model = {
-            'amplified': decode.AmplifiedDecode, 'scale': decode.ScaleDecode,
-            'resnet': decode.ResNetV2Decode, 'linear_conv': decode.LinearConvDecode
+            'amplified': decode.AmplifiedDecode,
+            'scale': decode.ScaleDecode,
+            'resnet': decode.ResNetV2Decode,
+            'linear_conv': decode.LinearConvDecode
         }[model_build_params.decode_model_key](model_build_params)
         value_model = {
-            'random': value.RandomValue, 'linear': value.Linear3DValue,
-            'linear_conv': value.LinearConvValue, 'cnn_lite': value.CnnLiteValue,
-            'resnet': value.ResNetV2Value, 'tromp_taylor': value.TrompTaylorValue
+            'random': value.RandomValue,
+            'linear': value.Linear3DValue,
+            'linear_conv': value.LinearConvValue,
+            'cnn_lite': value.CnnLiteValue,
+            'resnet': value.ResNetV2Value,
+            'tromp_taylor': value.TrompTaylorValue
         }[model_build_params.value_model_key](model_build_params)
         policy_model = {
-            'random': policy.RandomPolicy, 'linear': policy.Linear3DPolicy,
-            'linear_conv': policy.LinearConvPolicy, 'cnn_lite': policy.CnnLitePolicy,
-            'resnet': policy.ResNetV2Policy, 'tromp_taylor': policy.TrompTaylorPolicy
+            'random': policy.RandomPolicy,
+            'linear': policy.Linear3DPolicy,
+            'linear_conv': policy.LinearConvPolicy,
+            'cnn_lite': policy.CnnLitePolicy,
+            'resnet': policy.ResNetV2Policy,
+            'tromp_taylor': policy.TrompTaylorPolicy
         }[model_build_params.policy_model_key](model_build_params)
         transition_model = {
-            'real': transition.RealTransition, 'black_perspective': transition.BlackRealTransition,
-            'random': transition.RandomTransition, 'linear_conv': transition.LinearConvTransition,
-            'cnn_lite': transition.CnnLiteTransition, 'resnet': transition.ResNetV2Transition,
+            'real': transition.RealTransition,
+            'black_perspective': transition.BlackRealTransition,
+            'random': transition.RandomTransition,
+            'linear_conv': transition.LinearConvTransition,
             'resnet_action': transition.ResNetV2ActionTransition
         }[model_build_params.transition_model_key](model_build_params)
 
@@ -124,7 +144,8 @@ def build_model_transform(model_build_params: base.ModelBuildParams) -> hk.Multi
             value_logits = value_model(embedding)
             return decoding, value_logits, policy_logits, transition_logits
 
-        return init, (embed_model, decode_model, value_model, policy_model, transition_model)
+        return init, (embed_model, decode_model, value_model, policy_model,
+                      transition_model)
 
     return hk.multi_transform(f)
 
@@ -141,5 +162,7 @@ def save_model(params: optax.Params, model_dir: str):
         os.mkdir(model_dir)
     params_filename = os.path.join(model_dir, 'params.npz')
     with open(params_filename, 'wb') as params_file:
-        pickle.dump(jax.tree_util.tree_map(lambda x: x.astype('float32'), params), params_file)
+        pickle.dump(
+            jax.tree_util.tree_map(lambda x: x.astype('float32'), params),
+            params_file)
     print(f"Saved model to '{model_dir}'.")
--- src/muzero_gojax/models/transition.py
+++ src/muzero_gojax/models/transition.py
@@ -15,9 +15,11 @@ from muzero_gojax.models import embed
 class RandomTransition(base.BaseGoModel):
     """Outputs independent standard normal variables."""
 
-    def __call__(self, embeds):
-        return jax.random.normal(hk.next_rng_key(),
-                                 (len(embeds), *self.transition_output_shape[1:]), dtype='bfloat16')
+    def __call__(self, embeds, _=None):
+        return jax.random.normal(
+            hk.next_rng_key(),
+            (len(embeds), *self.transition_output_shape[1:]),
+            dtype='bfloat16')
 
 
 class RealTransition(base.BaseGoModel):
@@ -27,8 +29,9 @@ class RealTransition(base.BaseGoModel):
     Should be used with the identity embedding.
     """
 
-    def __call__(self, embeds):
-        return lax.stop_gradient(gojax.get_children(embeds.astype(bool)).astype('bfloat16'))
+    def __call__(self, embeds, _=None):
+        return lax.stop_gradient(
+            gojax.get_children(embeds.astype(bool)).astype('bfloat16'))
 
 
 class BlackRealTransition(base.BaseGoModel):
@@ -44,13 +47,15 @@ class BlackRealTransition(base.BaseGoModel):
         self._internal_black_perspective_embed = embed.BlackPerspectiveEmbed(
             *args, **kwargs)
 
-    def __call__(self, embeds):
+    def __call__(self, embeds, _=None):
         transitions = self._internal_real_transition(embeds)
         batch_size, action_size, channel, board_height, board_width = transitions.shape
         black_perspectives = self._internal_black_perspective_embed(
             jnp.reshape(transitions.astype(bool),
-                        (batch_size * action_size, channel, board_height, board_width)))
-        return lax.stop_gradient(jnp.reshape(black_perspectives, transitions.shape))
+                        (batch_size * action_size, channel, board_height,
+                         board_width)))
+        return lax.stop_gradient(
+            jnp.reshape(black_perspectives, transitions.shape))
 
 
 class LinearConvTransition(base.BaseGoModel):
@@ -58,44 +63,12 @@ class LinearConvTransition(base.BaseGoModel):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self._conv = hk.Conv2D(self.model_params.embed_dim * self.action_size, (3, 3),
+        self._conv = hk.Conv2D(self.model_params.embed_dim * self.action_size,
+                               (3, 3),
                                data_format='NCHW')
 
-    def __call__(self, embeds):
-        return jnp.reshape(self._conv(embeds.astype('bfloat16')), self.transition_output_shape)
-
-
-class CnnLiteTransition(base.BaseGoModel):
-    """
-    1-layer CNN model with hidden and output dimension.
-
-    Intended to be used the BlackCNNLite embedding.
-    """
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        odim = self.model_params.embed_dim * self.action_size
-        self._simple_conv_block = base.SimpleConvBlock(hdim=self.model_params.hdim, odim=odim,
-                                                       **kwargs)
-
-    def __call__(self, embeds):
-        return jnp.reshape(jax.nn.relu(self._simple_conv_block(embeds.astype('bfloat16'))),
-                           self.transition_output_shape)
-
-
-class ResNetV2Transition(base.BaseGoModel):
-    """ResNetV2 model."""
-
-    def __init__(self, *args, **kwargs):
-        # pylint: disable=duplicate-code
-        super().__init__(*args, **kwargs)
-        self._resnet = base.ResNetV2(hdim=self.model_params.hdim, nlayers=self.model_params.nlayers,
-                                     odim=self.model_params.hdim)
-        self._conv = hk.Conv2D(self.model_params.embed_dim * self.action_size, (1, 1),
-                               data_format='NCHW')
-
-    def __call__(self, embeds):
-        return jnp.reshape(self._conv(self._resnet(embeds.astype('bfloat16'))),
+    def __call__(self, embeds, _=None):
+        return jnp.reshape(self._conv(embeds.astype('bfloat16')),
                            self.transition_output_shape)
 
 
@@ -105,40 +78,48 @@ class ResNetV2ActionTransition(base.BaseGoModel):
     def __init__(self, *args, **kwargs):
         # pylint: disable=duplicate-code
         super().__init__(*args, **kwargs)
-        self._resnet = base.ResNetV2(hdim=self.model_params.hdim, nlayers=self.model_params.nlayers,
+        self._resnet = base.ResNetV2(hdim=self.model_params.hdim,
+                                     nlayers=self.model_params.nlayers,
                                      odim=self.model_params.hdim)
-        self._conv = hk.Conv2D(self.model_params.embed_dim,
-                               (1, 1), data_format='NCHW')
+        self._conv = hk.Conv2D(self.model_params.embed_dim, (1, 1),
+                               data_format='NCHW')
 
-    def __call__(self, embeds: jnp.ndarray, partial_actions: jnp.ndarray = None) -> jnp.ndarray:
+    def __call__(self,
+                 embeds: jnp.ndarray,
+                 partial_actions: jnp.ndarray = None) -> jnp.ndarray:
         # Embeds is N x D x B x B
         # We may only apply the model on a subset of actions A' in A.
 
         # A' x B x B
         if partial_actions is None:
             partial_actions = jnp.arange(self.action_size)
-        indicator_actions = gojax.action_1d_to_indicator(partial_actions,
-                                                         self.model_params.board_size,
-                                                         self.model_params.board_size)
+        indicator_actions = gojax.action_1d_to_indicator(
+            partial_actions, self.model_params.board_size,
+            self.model_params.board_size)
         # N x A' x 1 x B x B
         batch_size = len(embeds)
-        batch_indicator_actions = jnp.expand_dims(
-            jnp.repeat(jnp.expand_dims(indicator_actions, axis=0),
-                       repeats=batch_size, axis=0),
-            axis=2).astype('bfloat16')
+        batch_indicator_actions = jnp.expand_dims(jnp.repeat(
+            jnp.expand_dims(indicator_actions, axis=0),
+            repeats=batch_size,
+            axis=0),
+                                                  axis=2).astype('bfloat16')
         # N x A' x (D+1) x B x B
-        duplicated_embeds = jnp.repeat(jnp.expand_dims(embeds.astype('bfloat16'), axis=1),
-                                       repeats=self.action_size, axis=1)
+        duplicated_embeds = jnp.repeat(jnp.expand_dims(
+            embeds.astype('bfloat16'), axis=1),
+                                       repeats=self.action_size,
+                                       axis=1)
         embeds_with_actions = jnp.concatenate(
             (duplicated_embeds, batch_indicator_actions), axis=2)
 
         # N x A' x (D*)
         partial_transitions = nt_utils.unflatten_first_dim(
-            self._conv(self._resnet(
-                nt_utils.flatten_first_two_dims(embeds_with_actions))),
+            self._conv(
+                self._resnet(
+                    nt_utils.flatten_first_two_dims(embeds_with_actions))),
             batch_size, self.action_size)
 
         # N x A x (D*)
         transitions = jnp.zeros(
-            (batch_size, self.action_size, *embeds.shape[1:]), dtype=embeds.dtype)
+            (batch_size, self.action_size, *embeds.shape[1:]),
+            dtype=embeds.dtype)
         return transitions.at[:, partial_actions].set(partial_transitions)

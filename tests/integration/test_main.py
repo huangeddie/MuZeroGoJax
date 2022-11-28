f064dbe11b724fd65cbc8e53efe702c8ec72551c
Eddie Huang <aigagror@gmail.com>
2022-11-27 21:27:35 -0800

    Simplify test case

--- tests/integration/test_main.py
+++ tests/integration/test_main.py
@@ -32,9 +32,7 @@ class MainTestCase(chex.TestCase):
                          embed_model='IdentityEmbed',
                          transition_model='RealTransition',
                          value_model='TrompTaylorValue',
-                         policy_model='LinearConvPolicy',
-                         dtype='float32',
-                         loss_sample_action_size=26)
+                         policy_model='LinearConvPolicy')
     def test_real_linear_policy_learns_to_avoid_occupied_spaces(self):
         rng_key = jax.random.PRNGKey(FLAGS.rng)
         go_model, init_params = models.build_model_with_params(

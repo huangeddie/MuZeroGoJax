3b604dd2983fa2be03c4ab6aef6f68cbd27c2445
Eddie Huang <aigagror@gmail.com>
2022-12-23 15:40:18 -0800

    Add BroadcastResNetV2Embed to unit test

--- tests/unit/test_models.py
+++ tests/unit/test_models.py
@@ -231,6 +231,10 @@ class ModelsTestCase(chex.TestCase):
         dict(testcase_name=models.ResNetV2Embed.__name__,
              model_class=models.ResNetV2Embed,
              embed_dim=2,
+             expected_shape=(2, 2, 3, 3)),
+        dict(testcase_name=models.BroadcastResNetV2Embed.__name__,
+             model_class=models.BroadcastResNetV2Embed,
+             embed_dim=2,
              expected_shape=(2, 2, 3, 3)))
     def test_embed_model_output_type_and_shape(self, model_class, embed_dim,
                                                expected_shape):

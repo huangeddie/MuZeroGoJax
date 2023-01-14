29a3aed60d2c39854baf9e9056aeb9fe063c5656
Eddie Huang <aigagror@gmail.com>
2023-01-14 09:10:30 -0800

    Add test case for multi update steps

--- tests/unit/test_train.py
+++ tests/unit/test_train.py
@@ -17,6 +17,7 @@ class TrainCase(chex.TestCase):
     def setUp(self):
         FLAGS.mark_as_parsed()
 
+    # We need > 1 train steps because warmup scheduling causes first learning rate to be 0.
     @flagsaver.flagsaver(training_steps=2, board_size=3)
     def test_train_model_changes_params(self):
         rng_key = jax.random.PRNGKey(FLAGS.rng)
@@ -28,6 +29,26 @@ class TrainCase(chex.TestCase):
                                           FLAGS.dtype, rng_key)
         with self.assertRaises(AssertionError):
             chex.assert_trees_all_equal(params, new_params)
+
+    def test_multi_update_steps_params_differ_from_single_update_step_params(
+            self):
+        rng_key = jax.random.PRNGKey(FLAGS.rng)
+        all_models_build_config = models.get_all_models_build_config(
+            FLAGS.board_size, FLAGS.dtype)
+        go_model, params = models.build_model_with_params(
+            all_models_build_config, rng_key)
+        with flagsaver.flagsaver(training_steps=2, board_size=3):
+            single_update_params, _ = train.train_model(
+                go_model, params, FLAGS.board_size, FLAGS.dtype, rng_key)
+        with flagsaver.flagsaver(training_steps=2,
+                                 board_size=3,
+                                 updates_per_train_step=2):
+            two_update_params, _ = train.train_model(go_model, params,
+                                                     FLAGS.board_size,
+                                                     FLAGS.dtype, rng_key)
+        with self.assertRaises(AssertionError):
+            chex.assert_trees_all_equal(single_update_params,
+                                        two_update_params)
 
     @flagsaver.flagsaver(training_steps=2, board_size=3, eval_frequency=2)
     def test_train_model_sparse_eval_changes_params(self):
